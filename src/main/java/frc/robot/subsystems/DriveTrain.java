3e88199444f46198569aa2de441117c37be2a6ad
chmoore889 <chmoore889@gmail.com>
2019-09-27 22:32:46 -0400

    test2

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -6,13 +6,12 @@ import frc.robot.RobotMap;
 
 class DriveTrain<WPI_VictorSPX> {
 
-    VictorSPX frontRight, frontLeft, backRight, backLeft; 
+    VictorSPX frontRight, frontLeft, backRight, backLeft;
 
     public DriveTrain() {
-        frontRight = new VictorSPX (RobotMap.FRONT_RIGHT_DRIVE_TRAIN); 
-        frontLeft = new VictorSPX (RobotMap.FRONT_RIGHT_DRIVE_TRAIN); 
-        backRight = new VictorSPX (RobotMap.FRONT_RIGHT_DRIVE_TRAIN); 
-        backLeft = new VictorSPX (RobotMap.FRONT_RIGHT_DRIVE_TRAIN); 
-        
+        frontRight = new VictorSPX (RobotMap.FRONT_RIGHT_DRIVE_TRAIN);
+        frontLeft = new VictorSPX (RobotMap.FRONT_RIGHT_DRIVE_TRAIN);
+        backRight = new VictorSPX (RobotMap.FRONT_RIGHT_DRIVE_TRAIN);
+        backLeft = new VictorSPX (RobotMap.FRONT_RIGHT_DRIVE_TRAIN);
     }
 }
