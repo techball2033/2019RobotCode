d8cdd720dcdaa20bfe6dc0e30ea4498def77734d
chmoore889 <chmoore889@gmail.com>
2019-09-27 22:28:44 -0400

    test

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,16 @@
+package frc.robot; 
+
+public class RobotMap {
+
+    public static int FRONT_RIGHT_DRIVE_TRAIN = 1;
+    public static int FRONT_LEFT_DRIVE_TRAIN = 2;
+    public static int BACK_RIGHT_DRIVE_TRAIN = 3;
+    public static int BACK_LEFT_DRIVE_TRAIN = 4; 
+    
+    public static int ARM_RIGHT = 5;
+    public static int ARM_LEFT = 6;
+    public static int WRIST = 7;
+    public static int INTAKE = 8;
+
+    
+}
