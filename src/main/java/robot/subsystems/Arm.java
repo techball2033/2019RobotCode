fe704bf8af83e4b8c2b9753f661621d5ec677b84
chmoore889 <chmoore889@gmail.com>
2019-10-11 22:05:42 -0400

    Updated Arm & Wrist

--- src/main/java/robot/subsystems/Arm.java
+++ src/main/java/robot/subsystems/Arm.java
@@ -1,14 +1,21 @@
 package robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 public class Arm {
+
+    private final int potRange = ;//Range of pot in degrees
+    private final int potOffset = ;//Offset of pot from level
 
     private WPI_TalonSRX left;
     private WPI_TalonSRX right;
 
     private SpeedControllerGroup armGroup; 
+
+    private AnalogPotentiometer armPot;
 
     public Arm() {
         left = new WPI_TalonSRX(4);
@@ -17,6 +24,15 @@ public class Arm {
         right.setInverted(true);//Inverting one motor as they drive in different directions. CHECK WHICH SIDE TO INVERT.
 
         armGroup = new SpeedControllerGroup(left, right);
+
+        armPot = new AnalogPotentiometer(6, potRange, potOffset);
     }
 
+    public void override(double speed) {
+        armGroup.set(speed);
+    }
+
+    public void stopArm() {
+        armGroup.set(0);
+    }
 }
--- src/main/java/robot/subsystems/Wrist.java
+++ src/main/java/robot/subsystems/Wrist.java
@@ -1,13 +1,29 @@
 package robot.subsystems;
 
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.Spark;
 
 public class Wrist {
+
+    private final int potRange = ;//Range of pot in degrees
+    private final int potOffset = ;//Offset of pot from level
+
     private Spark wrist;
+
+    private AnalogPotentiometer armPot;
 
     public Wrist() {
         wrist = new Spark(1);
         wrist.setInverted(false);
+
+        armPot = new AnalogPotentiometer(7, potRange, potOffset);
     }
 
+    public void override(double speed) {
+        wrist.set(speed);
+    }
+
+    public void stopArm() {
+        wrist.set(0);
+    }
 }
