4c9dcb103122d676fc21d691af4f6de3a43a3e50
chmoore889 <chmoore889@gmail.com>
2019-10-29 20:02:41 -0400

    Removed redundancies

--- src/main/java/robot/subsystems/DriveTrain.java
+++ src/main/java/robot/subsystems/DriveTrain.java
@@ -23,10 +23,13 @@ public class DriveTrain {
         rightFront = new WPI_VictorSPX(2);
         rightBack = new WPI_VictorSPX(3);
 
+        //Redundant fix for drivetrain - put back in code if problems arise
+        /*
         leftFront.setSafetyEnabled(false);
         leftBack.setSafetyEnabled(false);
         rightFront.setSafetyEnabled(false);
         rightBack.setSafetyEnabled(false);
+        */
 
         leftSpeedGroup = new SpeedControllerGroup(leftFront,leftBack);
         rightSpeedGroup = new SpeedControllerGroup(rightFront,rightBack);
